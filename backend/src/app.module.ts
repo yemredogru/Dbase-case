bf42fa9ad1b216558307ae3eebe92d12a61d339a
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2024-01-25 12:59:12 +0300

    mongoosemodule deleted from app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,14 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import {MongooseModule} from '@nestjs/mongoose';
+import { DatabaseModule } from './database/database.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 @Module({
-  imports: [UserModule,MongooseModule.forRoot(process.env.MONGO_URI),],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
