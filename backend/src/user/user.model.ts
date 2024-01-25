1226522f2cd536cfc64fd695ee3d13274efe29f6
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2024-01-25 12:58:46 +0300

    phone validator added

--- backend/src/user/user.model.ts
+++ backend/src/user/user.model.ts
@@ -1,19 +1,24 @@
+import { MongoError } from 'mongodb';
 import * as mongoose from 'mongoose';
 import { Document } from 'mongoose';
 
 export const userSchema=new mongoose.Schema({
-  userName:{type:String,required:true},
+  userName:{type:String,required:true,unique:true},
   email:{type:String,required:true,unique:true},
   password:{type:String,required:true,min:8},
   birthDate:{type:Date,required:true},
-  phone:{type:String,required:true}
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (value: string) {
+        const phoneRegex = /^(?:[0-9] ?){6,14}[0-9]$/;
+        if (!phoneRegex.test(value)) {
+          throw new MongoError('Invalid phone number format');
+        }
+        return true;
+      },
+    },
+  },
 });
-
-
-export interface User extends Document{
-     userName: string;
-     email: string;
-     password: string;
-     birthDate:Date;
-     phone:String;
-}
